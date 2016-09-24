33d031da2435964d61716e33f6c96ea5a777e9fc
Matalayy <melihatalayy@gmail.com>
2016-09-24 22:20:04 +0300

    Create Memur.cs

---
+++ OOP/Inheritance/Memur.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Inheritance
+{
+    class Memur : Insan
+    {
+        public string SicilNo { get; set; }
+
+        public DateTime IseGirisTarihi { get; set; }
+
+        public decimal Maas { get; set; }
+
+        public string Derece { get; set; }
+
+    }
+}
