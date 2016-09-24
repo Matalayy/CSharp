a5a0e919ad661690e84b6f5b526fe1154058af7b
Matalayy <melihatalayy@gmail.com>
2016-09-24 22:19:49 +0300

    Create Insan.cs

---
+++ OOP/Inheritance/Insan.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Inheritance
+{
+    public class Insan
+    {
+        public string TCKN { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public DateTime DogumTarihi { get; set; }
+
+
+
+    }
+}
