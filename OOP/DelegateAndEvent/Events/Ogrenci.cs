11f294fb31c37720733f4df6e2ffa16c5c32729d
Matalayy <melihatalayy@gmail.com>
2016-09-24 22:04:49 +0300

    Create Ogrenci.cs

---
+++ OOP/DelegateAndEvent/Events/Ogrenci.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Events
+{
+    delegate void GectiKaldiEventHandler(object sender,GectiKaldiEventArgs e);
+
+    class GectiKaldiEventArgs:EventArgs
+    {
+        public string AdSoyad { get; set; }
+
+        public double Ortalama { get; set; }
+
+    }
+    class Ogrenci
+    {
+        public string Adi { get; set; }
+
+        public string Soyadi { get; set; }
+
+        private double vize;
+
+        public double Vize
+        {
+            get { return vize; }
+            set { vize = value; DurumKontrol(); }
+        }
+
+
+        private double final;
+
+        public double Final
+        {
+            get { return final; }
+            set { final = value; DurumKontrol(); }
+        }
+
+        void DurumKontrol()
+        {
+            if(Ortalama >= 70)
+            {
+                if(Gecti != null)
+                Gecti(this, new GectiKaldiEventArgs { AdSoyad = Adi + " " + Soyadi, Ortalama = Ortalama });
+            }
+            else
+            {
+                if(Kaldi != null)
+                Kaldi(this, new GectiKaldiEventArgs { AdSoyad = Adi + " " + Soyadi, Ortalama = Ortalama });
+            }
+        }
+
+
+        public double Ortalama
+        {
+            get { return Vize * 0.4 + Final * 0.6; }
+        }
+
+        public event GectiKaldiEventHandler Gecti;
+
+        public event GectiKaldiEventHandler Kaldi;
+
+        public override string ToString()
+        {
+            return Adi + " " + Soyadi + " " + Ortalama;
+        }
+    }
+}
