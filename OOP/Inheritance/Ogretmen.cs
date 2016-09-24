ba4e82b2ac8d960b6a06ddbf35f2b1ddddeb3ec4
Matalayy <melihatalayy@gmail.com>
2016-09-24 22:21:01 +0300

    Create Ogretmen.cs

---
+++ OOP/Inheritance/Ogretmen.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Inheritance
+{
+    class Ogretmen:Memur
+    {
+        public string Brans { get; set; }
+
+        public string Sinif { get; set; }
+
+        public int HaftalikDersSaati { get; set; }
+
+    }
+}
