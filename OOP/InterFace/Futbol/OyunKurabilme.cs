5179beaf3833e13169d6e924a254c8e4b9671bc1
Matalayy <melihatalayy@gmail.com>
2016-09-24 22:26:18 +0300

    Create OyunKurabilme.cs

---
+++ OOP/InterFace/Futbol/OyunKurabilme.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.InterFace
+{
+    interface OyunKurabilme
+    {
+        bool CalimAt(float hiz, float aci);
+
+    }
+}
