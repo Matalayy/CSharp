722979a0725c35e521f0f85b1f6db169e5567303
Matalayy <melihatalayy@gmail.com>
2016-09-24 22:31:43 +0300

    Create Insan.cs

---
+++ OOP/PolyMorphism/Insan.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.PolyMorphism
+{
+    class Insan
+    {
+        public string TCKN { get; set; }
+
+        public string Adi { get; set; }
+
+        public string SoyAdi { get; set; }
+
+        public virtual void InsanAl()
+        {
+            // insan almayi sagla
+        }
+
+    }
+}
