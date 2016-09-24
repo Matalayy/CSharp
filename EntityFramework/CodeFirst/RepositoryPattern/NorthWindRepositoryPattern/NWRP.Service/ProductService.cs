313445d15e9670550db6685b1afb4f0102e29db9
Matalayy <melihatalayy@gmail.com>
2016-09-24 23:44:18 +0300

    Add files via upload

---
+++ EntityFramework/CodeFirst/RepositoryPattern/NorthWindRepositoryPattern/NWRP.Service/ProductService.cs
@@ -0,0 +1,45 @@
+﻿using NorthWindRepositoryPatern.ORM.Models;
+using NWRP.DTO.Product;
+using NWRP.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWRP.Service
+{
+    public class ProductService
+    {
+        ProductRepository repository = new ProductRepository();
+        public IList<ProductListDTO> Listed()
+        {
+            return repository.Listed().Select(
+                x => new ProductListDTO
+                {
+                    ProductID = x.ProductID,
+                    ProductName = x.ProductName,
+                    SupplierID = x.SupplierID,
+                    CategoryID = x.CategoryID,
+                    QuantityPerUnit = x.QuantityPerUnit,
+                    UnitPrice = x.UnitPrice,
+                    UnitsInStock = x.UnitsInStock,
+                    UnitsOnOrder = x.UnitsOnOrder,
+                    ReorderLevel = x.ReorderLevel,
+                    Discontinued = x.Discontinued                    
+                }).ToList();
+        }
+
+        public void Add(ProductAddDTO entity)
+        {
+            Product product = new Product{
+                ProductName = entity.ProductName,
+                UnitPrice = entity.UnitPrice,
+                UnitsInStock = entity.UnitsInStock,
+                CategoryID = entity.CategoryId,
+                SupplierID = entity.SupplierId };
+
+            repository.Add(product);
+        }
+    }
+}
