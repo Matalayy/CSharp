7ec6e41ac0bac48ec8521fd4eeb9e54efd4e5d41
Matalayy <melihatalayy@gmail.com>
2016-09-24 23:45:46 +0300

    Add files via upload

---
+++ EntityFramework/CodeFirst/RepositoryPattern/NorthWindRepositoryPattern/NWRP.WinFormUI/Form1.cs
@@ -0,0 +1,27 @@
+﻿using NWRP.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NWRP.WinFormUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ProductService pservice = new ProductService();
+            dataGridView1.DataSource = pservice.Listed();
+        }
+    }
+}
